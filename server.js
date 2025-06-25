1ca3fc64791513bf7a34cb9f7fcb5826950a0e8a
Endhrajith T <endhrajitthiyagu@gmail.com>
2025-06-25 10:55:28 +0530

    inital buggy code is

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://endhrajiththiyagu:end123@cluster0.3wu5ebk.mongodb.net/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("MongoDB Error", err));
